6b35aa76c2605b2f49c0af84fdf6f2c7fedf517c
Michael Dryden <mjdryden@gmail.com>
2024-07-02 16:59:50 -0500

    Add support for virtual pushes

--- eventbus/virtual.go
+++ eventbus/virtual.go
@@ -2,12 +2,16 @@ package eventbus
 
 import (
 	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
 
 	"110yards.ca/libs/go/core/logger"
 )
 
 type VirtualPublisher struct {
-	topicName string
+	topicName  string
+	pushTarget string
 }
 
 // CreateSubscription implements Publisher.
@@ -24,16 +28,31 @@ func (v *VirtualPublisher) CreateTopic(topicName string) error {
 
 // Publish implements Publisher.
 func (v *VirtualPublisher) Publish(message interface{}) error {
-	// TODO: repo for messages
 	j, err := json.Marshal(message)
 	if err != nil {
 		return err
 	}
-	logger.Infof("Published message %s", string(j))
+
+	if v.pushTarget != "" {
+		// use http client to POST to target
+		_, err := http.NewRequest("POST", v.pushTarget, nil)
+		if err != nil {
+			return err
+		}
+	} else {
+		logger.Infof("Published virtual message %s (no push target set)", string(j))
+	}
 
 	return nil
 }
 
 func NewVirtualPublisher(topicName string) Publisher {
-	return &VirtualPublisher{topicName: topicName}
+	virtualPushKey := fmt.Sprintf("VIRTUAL_PUSH_%s", topicName)
+	pushTarget := os.Getenv(virtualPushKey)
+
+	if pushTarget == "" {
+		logger.Warnf("No virtual push target found for topic %s, configure %s to enable push in dev", topicName, virtualPushKey)
+	}
+
+	return &VirtualPublisher{topicName: topicName, pushTarget: pushTarget}
 }
